c10905ad8ba44753c768f4d882df5d0021312ad1
Shavleen <shavleen.singh05@gmail.com>
2023-12-08 19:29:43 -0800

    fixed merge issues - last commit

--- frontend/Expense/Expenses.js
+++ frontend/Expense/Expenses.js
@@ -1,32 +1,3 @@
-<<<<<<< Updated upstream
-import {
-  Image,
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-  useWindowDimensions,
-} from "react-native";
-import Animated, { useSharedValue, withTiming } from "react-native-reanimated";
-import { Link } from "@react-navigation/native";
-import Sidebar from "./sidebar";
-import SectionView from "./section-view";
-import Heading1 from "./heading1";
-
-export default function Expenses() {
-  const { height, width } = useWindowDimensions();
-  const styles = makeStyles(width);
-
-  return (
-    <View style={styles.container}>
-      <Sidebar page="expenses" />
-      <SectionView>
-        <Heading1>Expenses</Heading1>
-        <View style={styles.subContainer} />
-        <View style={styles.subContainer1} />
-        <View style={styles.subContainer2} />
-        <View style={styles.subContainer3} />
-=======
 import React, { useState } from "react";
 import { View, StyleSheet, Text, TextInput, Pressable, Button, ScrollView } from "react-native";
 import Sidebar from "./sidebar";
@@ -382,55 +353,11 @@ function handleChange(name, value) {
               ))}
           </View>
         </ScrollView>
->>>>>>> Stashed changes
       </SectionView>
     </View>
   );
 }
 
-<<<<<<< Updated upstream
-const makeStyles = (width) =>
-  StyleSheet.create({
-    container: {
-      flexDirection: width >= 1200 ? "row" : "column",
-      alignContent: width >= 1200 ? "space-between" : "",
-      height: "100%",
-      width: "100%",
-    },
-
-    subContainer: {
-      flexDirection: "row",
-      alignContent: "space-between",
-    },
-    subContainer1: {
-      width: 100,
-      height: 100,
-      margin: 20,
-      flex: 1,
-      padding: 10, // Adjust padding as needed
-      backgroundColor: "#ddd",
-      borderRadius: 32,
-    },
-    subContainer2: {
-      width: 100,
-      height: 100,
-      margin: 20,
-      flex: 2,
-      padding: 10, // Adjust padding as needed
-      backgroundColor: "#ddd",
-      borderRadius: 32,
-    },
-    subContainer3: {
-      width: 100,
-      height: 100,
-      margin: 20,
-      flex: 1,
-      padding: 10, // Adjust padding as needed
-      backgroundColor: "#ddd",
-      borderRadius: 32,
-    },
-  });
-=======
 
 const styles = StyleSheet.create({
   container: {
@@ -548,4 +475,3 @@ const styles = StyleSheet.create({
     },
   });
   
->>>>>>> Stashed changes
